e36c9c37e7d80facd4cd38b000e7609de5e956c7
susmithapathuri <susmithapathuri@10.169.225.201>
2019-02-25 15:11:37 +0100

    First Commit

--- PatternRecognitionTest/src/com/pattern/application/PatternRecognitionClass.java
+++ PatternRecognitionTest/src/com/pattern/application/PatternRecognitionClass.java
@@ -1,16 +1,25 @@
-package com.assignment.patternRecognition;
+package com.pattern.application;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-@Path("/Pattern")
-public class PatternRecognition {
+@Path("/pattern")
+public class PatternRecognitionClass {
 
-	
+	@GET
+	@Path("/{param}")
+	public Response getMsg(@PathParam("param") String msg) {
+		String output = "WelcomE" +msg;
+		return Response.status(200).entity(output).build();
+		
+	}
 	@GET
 	@Path("/space")
 	@Produces(MediaType.TEXT_PLAIN)
@@ -28,11 +37,11 @@ public class PatternRecognition {
 	}
 	
 	@POST
-	@Path("{points}")
-	@Produces(MediaType.TEXT_PLAIN)
-	public String addPointToSpace(@PathParam("points") String points)
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response addPointToSpace(@FormParam("x") String x, @FormParam("y") String y)
 	{
-		return null;
+		return Response.status(200).entity(" Product added successfuly!<br> X: "+x+"<br> Y: " + y).build(); 
 		
 	}
 	
---
+++ PatternRecognitionTest/src/com/pattern/service/Space.java
@@ -0,0 +1,5 @@
+package com.pattern.service;
+
+public class Space {
+
+}
